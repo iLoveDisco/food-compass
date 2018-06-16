b04c4dd8975bf0605d9d3357a47f7db78eca1627
Sam Craig <craigsamm@gmail.com>
2018-06-16 08:59:25 -0400

    Refactor and comment code

--- script/map.js
+++ script/map.js
@@ -1,31 +1,37 @@
-// database is the giant JSON list of the database
+// database is the giant JSON list of the database (loaded in file database.js)
 
+// Where we will input the info onto the page.
 var infoBox = document.getElementById("info")
 
-var mymap = L.map('mapid').setView([39.759135, -86.158368], 14.3);
+// Create the map display.
+var map = L.map('map').setView([39.759135, -86.158368], 14.3)
 L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     id: 'mapbox.streets',
     accessToken: 'pk.eyJ1Ijoic2FtbWlkeXNhbSIsImEiOiJjamlnb2RxMXUxN2c1M3Byd3cyNDVkOWl2In0.Is8dM6T4w7eJHydcajXhzA'
-}).addTo(mymap);
+}).addTo(map);
 
+// Locate the user.
 var lc = L.control.locate({
   position: "topleft",
   keepCurrentZoomLevel: true,
   strings: {
     title: "Determine location"
   }
-}).addTo(mymap);
+}).addTo(map);
 lc.start();
 
-// Stolen from https://stackoverflow.com/questions/43167417/calculate-distance-between-two-points-in-leaflet (thanks)
+// Stolen from https://stackoverflow.com/questions/43167417/calculate-distance-between-two-points-in-leaflet
+// These calculate various distances between coordinates
 function toRadian(degree) {
   return degree*Math.PI/180;
 }
+
 function metersToMiles(meters) {
   return meters / 1600
 }
+
 function getDistance(origin, destination) {
   // return distance in meters
   var lon1 = toRadian(origin[1]),
@@ -41,9 +47,12 @@ function getDistance(origin, destination) {
   var EARTH_RADIUS = 6371;
   return c * EARTH_RADIUS * 1000;
 }
+
 function twoDecimalPlaceMileDistance (origin, destination) {
   return Math.round(100 * metersToMiles(getDistance(origin, destination))) / 100
 }
+
+// The text that will be put into the info box.
 function infoText (marker, origin, destination) {
   return `<p>
   ${marker.agency.name}<br />
@@ -59,15 +68,15 @@ function infoText (marker, origin, destination) {
   </p>`
 }
 
+// Get the agency locations, and store them into markers
 var provider = new window.GeoSearch.EsriProvider();
-
 var myCoordinates = []
-
 var markers = []
+
 database.forEach(function (agency) {
   provider.search({ query: agency.address + ", Indianapolis, IN" }).then(function (result) {
     var marker = L.marker([result[0].y, result[0].x])
-    marker.addTo(mymap)
+    marker.addTo(map)
     marker.agency = agency
     markers.push(marker)
 
@@ -80,6 +89,8 @@ database.forEach(function (agency) {
   })
 })
 
+// When we find our location, store information in the markers
+// and store our location
 function onLocationFound (e) {
   myCoordinates = []
   myCoordinates.push(e.latlng.lat)
@@ -93,4 +104,4 @@ function onLocationFound (e) {
   })
 }
 
-mymap.on('locationfound', onLocationFound)
+map.on('locationfound', onLocationFound)
